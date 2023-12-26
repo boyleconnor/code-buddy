f5003ac2b49dfcbd7027a033d84b12c332118a73
Connor Boyle <connor.bo@gmail.com>
2023-12-25 22:16:15 -0800

    Change git clone script to take list of repos

--- clone_repos.py
+++ clone_repos.py
@@ -1,51 +1,34 @@
 """Copied from: https://github.com/sayakpaul/hf-codegen/blob/main/data/parallel_clone_repos.py"""
+import argparse
 import os
 import subprocess
 from multiprocessing import Pool
-from github import Github
 
-ORG = "huggingface"
-MIRROR_DIRECTORY = "hf_public_repos"
-
-
-def get_repos(username, access_token=None, include_fork=False):
-    """Fetches repositories for a particular GitHub user.
-
-    Courtesy: Chansung Park.
-    """
-    g = Github(access_token)
-    user = g.get_user(username)
-
-    results = []
-    for repo in user.get_repos():
-        if repo.fork is False:
-            results.append(repo.name)
-        else:
-            if include_fork is True:
-                results.append(repo.name)
-
-    return results
+LOCAL_DIRECTORY = "hf_public_repos"
 
 
 def mirror_repository(repository):
     """Locally clones a repository."""
-    repository_url = f"https://github.com/{ORG}/{repository}.git"
-    repository_path = os.path.join(MIRROR_DIRECTORY, repository)
+    repository_url = f"https://github.com/{repository}.git"
+    repository_path = os.path.join(LOCAL_DIRECTORY, repository)
 
+    # FIXME: This clones the repo instead of downloading it
     # Clone the repository
     subprocess.run(["git", "clone", repository_url, repository_path])
 
 
 def mirror_repositories():
-    # Create the mirror directory if it doesn't exist
-    if not os.path.exists(MIRROR_DIRECTORY):
-        os.makedirs(MIRROR_DIRECTORY)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("repositories", type=str, nargs="+")
+    args = parser.parse_args()
 
-    # Get the list of repositories in the organization
-    if not os.environ["GH_ACCESS_TOKEN"]:
-        raise ValueError("You must set `GH_ACCESS_TOKEN` as an env variable.")
-    repositories = get_repos(ORG, os.environ["GH_ACCESS_TOKEN"])
-    print(f"Total repositories found: {len(repositories)}.")
+    # Get the list of repositories
+    repositories: list[str] = args.repositories
+    print(f"Total repositories: {len(repositories)}.")
+
+    # Create the mirror directory if it doesn't exist
+    if not os.path.exists(LOCAL_DIRECTORY):
+        os.makedirs(LOCAL_DIRECTORY)
 
     # Mirror repositories using multiprocessing
     print("Cloning repositories.")
