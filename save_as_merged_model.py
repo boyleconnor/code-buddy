7974ffa31f66c990c0b3f7933f58db73182781e1
Connor Boyle <connor.bo@gmail.com>
2024-01-13 08:56:06 -0800

    Add script to merge & save fine-tuned model

--- save_as_merged_model.py
+++ save_as_merged_model.py
@@ -6,21 +6,21 @@ from transformers import (
 )
 
 from peft import (
-    PeftModel
+    PeftModel, PeftModelForCausalLM
 )
 
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype="bfloat16",
-    bnb_4bit_use_double_quant=False,
-)
+# bnb_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_compute_dtype="bfloat16",
+#     bnb_4bit_use_double_quant=False,
+# )
 
 device_map = {"": 0}
 model = "bigcode/starcoderbase-1b"
 tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(
-    model, quantization_config=bnb_config,
+    model, quantization_config=None,
     device_map=device_map,
     trust_remote_code=True, 
     torch_dtype=torch.bfloat16,
@@ -28,6 +28,11 @@ model = AutoModelForCausalLM.from_pretrained(
 
 model_id = "peft-lora-starcoder15B-v2-personal-copilot-full"
 model = PeftModel.from_pretrained(model, model_id, adapter_name="copilot")
+print(type(model))
+model = model.merge_and_unload()
+print(type(model))
+
+model.save_pretrained("merged-peft-lora-starcoder")
 
 
 if not hasattr(model, "hf_device_map"):
